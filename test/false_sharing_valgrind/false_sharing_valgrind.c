327c3e7db8706ef1eb8b7635753d75c3e6a3dbeb
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2017-11-21 14:57:39 +0100

    hej

--- test/false_sharing_valgrind/false_sharing_valgrind.c
+++ test/false_sharing_valgrind/false_sharing_valgrind.c
@@ -1,24 +1,79 @@
 #include <stdio.h>
+#include <limits.h>
+
 #include <pthread.h>
 
-int	z;
-int y;
+#include "timer.h"
 
-static int x;
+#define CACHE_LINE_SIZE	(64)
+//#define LOOP_LENGTH		(INT_MAX / 10)
+#define LOOP_LENGTH		(100)
 
-void* a(void* args)
+/*	Normal structure, a and b will probably exist in the same 
+	cache block */
+typedef struct {
+	int	a;
+	int	b;
+} bad_t;
+
+/*	By inserting padding to make a and b reside in different
+	cache blocks we avoid false sharing */
+typedef struct {
+	int		a;
+	char	pad[CACHE_LINE_SIZE];
+	int		b;
+} good_t;
+
+static bad_t	b;
+static good_t	g;
+
+void* sum_a(void* args)
 {
-	if (x == 0)
-		z = 1;
-
+	int	s;
+	int	i;
+	
+	if (args) { }
+	
+	for (i = 0; i < LOOP_LENGTH; i++)
+		s += b.a;
+		
 	return NULL;
 }
 
-void* b(void* args)
+void* inc_b(void* args)
 {
-	if (x == 0)
-		y = 1;
+	int	i;
+	
+	if (args) { }
 
+	for (i = 0; i < LOOP_LENGTH; i++)
+		b.b++;
+		
+	return NULL;
+}
+
+void* sum_a_good(void* args)
+{
+	int	s;
+	int	i;
+
+	if (args) { }
+
+	for (i = 0; i < LOOP_LENGTH; i++)
+		s += g.a;
+		
+	return NULL;
+}
+
+void* inc_b_good(void* args)
+{
+	int	i;
+	
+	if (args) { }
+
+	for (i = 0; i < LOOP_LENGTH; i++)
+		g.b++;
+		
 	return NULL;
 }
 
@@ -26,11 +81,21 @@ int main(void)
 {
 	pthread_t	threads[2];
 	
-	pthread_create(&threads[0], NULL, a, NULL);
-	pthread_create(&threads[1], NULL, b, NULL);
+	start_timer();
+	pthread_create(&threads[0], NULL, sum_a, NULL);
+	pthread_create(&threads[1], NULL, inc_b, NULL);
 	
 	pthread_join(threads[0], NULL);
 	pthread_join(threads[1], NULL);
+	printf("b: %1.2fs\n", stop_timer());
+	
+	start_timer();
+	pthread_create(&threads[0], NULL, sum_a_good, NULL);
+	pthread_create(&threads[1], NULL, inc_b_good, NULL);
+	
+	pthread_join(threads[0], NULL);
+	pthread_join(threads[1], NULL);
+	printf("g: %1.2fs\n", stop_timer());
 	
 	return 0;
 }
--- valgrind-3.13.0/falsegrind/fg_main.c
+++ valgrind-3.13.0/falsegrind/fg_main.c
@@ -8,67 +8,97 @@
 #include "pub_tool_machine.h"
 #include "pub_tool_mallocfree.h"
 #include "pub_tool_threadstate.h"
+#include "pub_tool_addrinfo.h"
 
+/*
+************************************************
+ENUMS, STRUCTS AND THINGS TO MOVE TO OTHER FILES
+************************************************
+*/
+
+/* Types of memory accesses */
 typedef enum {
-	MEM_STORE,
-	MEM_LOAD,
-	MEM_STORE_GUARDED,
-	MEM_LOAD_GUARDED
+	MEM_STORE,			/* Ordinary store */
+	MEM_STORE_GUARDED,	/* Guarded store */
+	MEM_LOAD,			/* Ordinary load */
+	MEM_LOAD_GUARDED	/* Guarded load */
 } memory_access_e_t;
 
-/*	Strucure needs:
-	THREAD ID
-	ADDRESS
-	SIZE OF ACCESS 
-	LINE OF CODE 
-	FILENAME?
-	DIRNAME?
-*/
+/*	Holds a memory access made by a thread */
 typedef struct {
-	HChar*				file;
-	HChar*				dir;
-	IRExpr*				addr;
-	SizeT				size;
-	memory_access_e_t	access;
-	UInt				line;
-	IRType				type;
-//	ThreadId			tid;
+	HChar*				file;	/* Which file the memory access is at */
+	HChar*				dir;	/* Which directory ^ */
+	HChar*				var;	/* Variable name */
+	Addr				addr;	/* Which address the memory access points to */
+	SizeT				size;	/* Size of memory access, or variable */
+	memory_access_e_t	access;	/* Type of memory access, see enum above */
+	UInt				line;	/* At which line */
 } thread_access_t;
 
 /*	Basic vector for C. */
 typedef struct {
-	thread_access_t**	data;
-	unsigned long long	reserved;
-	unsigned long long	count;
+	void**	data;					/* Array of pointers to the elements */
+	unsigned long long	reserved;	/* Current reserved space to the data array */
+	unsigned long long	count;		/* Actually filled spots, count <= reserved */
 } vector_t;
 
-static void init_vector(vector_t* vector)
+static vector_t* new_vector(void)
 {
-	vector->count = 0;
-	vector->reserved = 1;
+	vector_t*	vector = VG_(malloc)("new_vector_vector", sizeof(vector_t));
 	
-	vector->data = VG_(malloc)("init_vector_vector_data", vector->reserved * sizeof(thread_access_t*));
+	vector->reserved = 1;
+	vector->count = 0;
+	vector->data = VG_(malloc)("new_vector_vector_data", vector->reserved * sizeof(void*));
+	
+	return vector;
 }
+
 static void increase_capacity(vector_t* vector)
 {
-	thread_access_t**	new_data;
+	void**	new_data;
 		
-	new_data = VG_(realloc)("vector->data", vector->data, vector->reserved * 2 * sizeof(thread_access_t*));
+	new_data = VG_(realloc)("increase_capacity_vector_data", vector->data, vector->reserved * 2 * sizeof(void*));
 	
 	vector->reserved *= 2;
 	vector->data = new_data;
 }
 
-static void insert_last(vector_t* vector, thread_access_t* access)
+static void insert_last(vector_t* vector, void* element)
 {
 	if (vector->count >= vector->reserved)
 		increase_capacity(vector);
 			
-	vector->data[vector->count++] = access;
+	vector->data[vector->count++] = element;
 }
 
-static thread_access_t* get(vector_t* vector, unsigned long long index)
+static Bool insert_last_exclusive(vector_t* vector, HChar* element)
 {
+	unsigned long long	i;
+	HChar*				tmp;
+		
+	for (i = 0; i < vector->count; i++) {
+		tmp = vector->data[i];
+		
+		if (VG_(strcmp)(tmp, element) == 0)
+			return False;
+	}
+	
+	insert_last(vector, element);
+	
+	return True;
+}
+
+static void* get(vector_t* vector, unsigned long long index, Bool nested)
+{
+	vector_t*	intermediate;
+	
+	if (nested) {
+		while (index >= vector->count) {
+			intermediate = new_vector();
+			insert_last(vector, intermediate);
+		}
+	}
+	
 	return vector->data[index];
 }
 
@@ -77,95 +107,199 @@ static unsigned long long size(vector_t* vector)
 	return vector->count;
 }
 
-static HChar*	g_filename;
-static HChar*	g_dirname;
-
-static vector_t	accesses[3 /* Change this to nbr of threads */];
-
-static Int check_collisions(ThreadId tid, thread_access_t* access)
+static void free_vector(vector_t* vector)
 {
-	vector_t*			vector;
-	thread_access_t*	current;
-	unsigned long long 	count;
-	unsigned long long 	j;
-	Int					nbr_threads;
-	Int					ignore;
-	Int					i;
+	unsigned long long	i;
+	unsigned long long	n;
 	
-	nbr_threads = 3;
-	ignore = tid - 1;
+	n = vector->count;
 	
-	for (i = 0; i < nbr_threads; i++) {
-		if (i == ignore)
+	for (i = 0; i < n; i++)
+		VG_(free)(vector->data[i]);
+		
+	VG_(free)(vector->data);
+	VG_(free)(vector);
+}
+
+/*
+****************
+GLOBAL VARIABLES
+****************
+*/
+
+static Bool			fg_allow_empty_filename = True;
+
+//	TODO: Replace this with a e.g hash table
+static vector_t*	g_accesses;
+static vector_t*	g_collisions;
+static HChar*		g_filename;
+static HChar*		g_dirname;
+static UInt			g_cache_alignment = 64;	/*	CHANGE THIS LATER */
+
+/*
+********************
+FALSEGRIND FUNCTIONS
+********************
+*/
+
+static Bool access_in_range(thread_access_t* first, thread_access_t* second)
+{
+	long long difference;
+	
+	if (first->addr == second->addr)
+		return False;	/* Ignore possible data race, not our job */
+		
+	if (first->access >= MEM_LOAD && first->access <= MEM_LOAD_GUARDED)
+		if (second->access >= MEM_LOAD && second->access <= MEM_LOAD_GUARDED)
+			return False;	/* Ignore two loads */
+		
+	difference = first->addr - second->addr;
+	
+	if (difference < 0)
+		difference *= (-1);
+			
+	if (difference >= g_cache_alignment)
+		return False;	/* Does not fit into same cache block */
+	
+	if (VG_(strcmp)(first->file, second->file) != 0)
+		return False;	/* Not even same file */
+		
+	return True;
+}
+
+UInt int_length(UInt nbr)
+{
+	UInt	n;
+	
+	for (n = 0; nbr > 0; nbr /= 10)
+		n++;
+		
+	return n;
+}
+
+HChar* int_to_char_array(UInt nbr)
+{
+	HChar*	result;
+	UInt	i;
+	UInt	n;
+	
+	n = int_length(nbr);
+	result = VG_(malloc)("", sizeof(HChar) * n);
+	
+	for (i = 0; i < n; i++) {
+		result[i] = nbr % 10;
+		nbr /= 10;
+	}
+	
+	return result;
+}
+
+/*	This is probably slow, opt for hash table later */
+static void check_false_sharing(thread_access_t* access, ThreadId tid)
+{
+	thread_access_t*	tmp;
+	vector_t*			tmp_vector;
+	unsigned long long	collisions;
+	//vector_t*			lines;
+	ThreadId			i;
+	unsigned long long	j;
+	ThreadId			thread_count;
+	unsigned long long	element_count;
+	HChar*				tmp_collision_info;
+	
+	UInt				file_len;
+	UInt				var_len;
+	UInt				a_line_len;
+	UInt				t_line_len;
+	
+	thread_count = size(g_accesses);
+	collisions = 0;
+	//lines = new_vector();
+	
+	/* Thread 0 does not exist, starts from 1 */
+	for (i = 1; i < thread_count; i++) {
+		if (i == tid)	// Can't get collisions with same thread
 			continue;
 			
-		vector = &accesses[i];
-		count = size(vector);
+		tmp_vector = get(g_accesses, i, True);
+		element_count = size(tmp_vector);
 		
-		for (j = 0; j < count; j++) {
-			current = get(vector, j);
-			
-			if (current->access != MEM_STORE)
-				continue;
-			
-			long long a = (long long)access->addr;
-			long long b = (long long)current->addr;
-			
-			VG_(printf)("distance: %d %d %d %d %d\n", a - b, a, b, current->line, access->line);
-			
-			if (current->addr == access->addr) {
+		for (j = 0; j < element_count; j++) {
+			tmp = get(tmp_vector, j, False);
+						
+			if (access_in_range(access, tmp)) {
+				collisions++;
 				
-					
+				file_len = VG_(strlen)(access->file);
+				var_len = VG_(strlen)(access->var);
+				a_line_len = int_length(access->line);
+				t_line_len = int_length(tmp->line);
 				
-					
-				VG_(printf)("found match t %d %d %04X %d %d\n", i + 1, tid, access->addr, current->line, access->line);
+				tmp_collision_info = VG_(malloc)("", sizeof(HChar) * (file_len + var_len + a_line_len + t_line_len + 1));
+				VG_(memcpy)(tmp_collision_info, access->file, file_len);
+				VG_(memcpy)(tmp_collision_info + file_len, access->var, var_len);
+				VG_(memcpy)(tmp_collision_info + file_len + var_len, int_to_char_array(access->line), a_line_len);
+				VG_(memcpy)(tmp_collision_info + file_len + var_len + a_line_len, int_to_char_array(tmp->line), t_line_len);
+				tmp_collision_info[file_len + var_len + a_line_len + t_line_len] = '\0';
 				
-				if (access->addr->tag == Iex_Unop) {
-					VG_(printf)("%d\n", access->addr->Iex.Unop.arg->Iex.Const.con->Ico.U32);
-				}
+				if (insert_last_exclusive(g_collisions, tmp_collision_info))
+					VG_(printf)("false sharing possible in \"%s\" in variable \"%s\" with lines %d vs %d\n", access->file, access->var, access->line, tmp->line);
+				else
+					VG_(free)(tmp_collision_info);
 				
-				return 1;
+				/*
+				tmp_line = VG_(malloc)("check_false_sharing", sizeof(UInt));
+				*tmp_line = tmp->line;
+
+				//VG_(printf)("false sharing possible in %s:%d vs %s:%d\n", access->file, access->line, tmp->file, tmp->line);
+				
+				insert_last(lines, tmp_line);
+				*/
 			}
 		}
 	}
 	
-	return 0;
-	
 	/*
-	Int					ignore;
-	thread_access_t*	current;
-	unsigned long long	i;
-	unsigned long long 	count;
-	
-	count = size(vector);
-	ignore = tid - 1;
-	
-	for (i = 0; i < count; i++) {
-		current = get(vector, i);
-		
-		if (current->addr == access->addr)
-			VG_(printf)("holy nigger same address\n");
+	if (collisions > 0) {
+		for (i = 0; i < size(lines); i++)
+			VG_(printf)("false sharing might occur in line %d vs line %d\n", access->line, *(UInt*)get(lines, i, False));
 	}
-	
-	return 0;
+		
+	free_vector(lines);
 	*/
 }
 
-static VG_REGPARM(1) void add_to_vector(thread_access_t* addr)
+static VG_REGPARM(2) void add_to_vector(thread_access_t* access, Addr a)
 {
-	
 	ThreadId	tid;
+	AddrInfo 	ai;
 	
 	tid = VG_(get_running_tid)();
-	//tid = addr->tid;
+	VG_(describe_addr)(a, &ai);
 	
-	/*
-	if (check_collisions(tid, addr))
-		VG_(printf)("FALSE SHARING IS HAPPENING line: %d\n", addr->line);
-		*/
+	if (ai.tag != Addr_DataSym)
+		return;
 		
-	insert_last(&accesses[tid - 1], addr);
-	//VG_(printf)("actually running this %zu from thread %d\n", accesses[0].count, addr->tid);
+	/*
+	if (tid != addr->tid)
+		VG_(printf)("***** NOT SAME TID *****\ntid: %d vs addr->tid: %d\n***** NOT SAME TID *****\n", tid, addr->tid);
+	*/
+	
+	access->addr = a;
+	access->var = VG_(malloc)("add_to_vector_access->var", sizeof(HChar) * (VG_(strlen)(ai.Addr.DataSym.name) + 1));
+	VG_(strcpy)(access->var, ai.Addr.DataSym.name);
+	
+	#if 0
+	VG_(printf)("a is %lx, var name: %s, thread: %d\n", access->addr, ai.Addr.DataSym.name, tid);
+	
+	if (access->access == MEM_STORE || access->access == MEM_STORE_GUARDED)
+		VG_(printf)("and it was a store\n");
+	else
+		VG_(printf)("and it was a load\n");
+	#endif
+		
+	insert_last(get(g_accesses, tid, True), access);
+	check_false_sharing(access, tid);
 }
 
 static void add_access(memory_access_e_t access, IRSB* sb, IRType type /* also includes size */, IRExpr* guard, ThreadId tid, IRExpr* addr, HChar* file, UInt line)
@@ -174,24 +308,31 @@ static void add_access(memory_access_e_t access, IRSB* sb, IRType type /* also i
 	IRExpr**			argv;
 	thread_access_t*	t_a;
 	
+	/*
+	//	TODO: Change this to general case obviously
 	if (VG_(strcmp)(file, "false_sharing_valgrind.c") != 0)
 		return;
+	*/
 	
 	t_a = VG_(malloc)("add_access_t_a", sizeof(thread_access_t));
-	//VG_(printf)("type: %04X\n", type);
-	
-	tid = VG_(get_running_tid)();
-	
+		
 	t_a->addr = addr;
 	t_a->access = access;
 	t_a->file = NULL;
 	t_a->dir = NULL;
-	t_a->size = 4 /* Change this obviously */;
-	t_a->line = line /* This too */;
-	t_a->type = type;
-	//t_a->tid = tid;
-		
-	argv = mkIRExprVec_1(mkIRExpr_HWord((HWord)t_a));
+	t_a->line = line;
+	
+	t_a->file = VG_(malloc)("add_access_t_a_file", sizeof(HChar) * (VG_(strlen)(g_filename) + 1));
+	VG_(strcpy)(t_a->file, g_filename);
+	
+	if (type == Ity_I32)
+		t_a->size = 4;
+	else if (type == Ity_I64)
+		t_a->size = 8;
+	else
+		t_a->size = 0;
+	
+	argv = mkIRExprVec_2(mkIRExpr_HWord((HWord)t_a), addr);
 	di = unsafeIRDirty_0_N(1, "add_to_vector", VG_(fnptr_to_fnentry)(&add_to_vector), argv);
 	
 	if (guard)
@@ -200,9 +341,13 @@ static void add_access(memory_access_e_t access, IRSB* sb, IRType type /* also i
 	addStmtToIRSB(sb, IRStmt_Dirty(di));
 }
 
-static void fg_post_clo_init(void)
-{
-}
+/*
+****************************
+VALGRIND SPECIFIED FUNCTIONS
+****************************
+*/
+
+static void fg_post_clo_init(void) {}
 
 static IRSB* fg_instrument(VgCallbackClosure* closure, IRSB* sbIn, const VexGuestLayout* layout, const VexGuestExtents* vge, const VexArchInfo* archinfo, IRType gWordTy, IRType hWordTy)
 {
@@ -220,12 +365,11 @@ static IRSB* fg_instrument(VgCallbackClosure* closure, IRSB* sbIn, const VexGues
 	if (gWordTy != hWordTy)
 		VG_(tool_panic)("host/guest word size does not match");
 		
-	if (!VG_(get_filename_linenum)(closure->nraddr, &g_filename, &g_dirname, &line))
+	if (!VG_(get_filename_linenum)(closure->nraddr, &g_filename, &g_dirname, &line)) {
+		//g_filename = NULL;
+		//g_dirname = NULL;
 		line = 0;
-		
-		
-	if (line == 21)
-		VG_(printf)("FOUND\n");
+	}
 		
 	sbOut = deepCopyIRSBExceptStmts(sbIn);
 	
@@ -329,6 +473,7 @@ static IRSB* fg_instrument(VgCallbackClosure* closure, IRSB* sbIn, const VexGues
 
 static void fg_fini(Int exitcode)
 {	
+	/*
 	Int	i;
 	Int	j;
 	
@@ -337,6 +482,7 @@ static void fg_fini(Int exitcode)
 			check_collisions(i + 1, get(&accesses[i], j));
 		}
 	}
+	*/
 	
 	VG_(printf)("Compiled %s at %s\n", __DATE__, __TIME__);
 }
@@ -355,9 +501,8 @@ static void fg_pre_clo_init(void)
 	g_filename = VG_(malloc)("g_filename", sizeof(HChar) * 255);
 	g_dirname = VG_(malloc)("g_dirname", sizeof(HChar) * 255);
 	
-	init_vector(&accesses[0]);
-	init_vector(&accesses[1]);
-	init_vector(&accesses[2]);
+	g_accesses = new_vector();
+	g_collisions = new_vector();
 }
 
 VG_DETERMINE_INTERFACE_VERSION(fg_pre_clo_init)
